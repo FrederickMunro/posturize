388058114172487ab519d63dc4a97450903ed148
Mah514 <72531358+Mah514@users.noreply.github.com>
2022-04-10 20:23:22 -0400

    Added user profile simple CSS

--- screens/Home.js
+++ screens/Home.js
@@ -3,7 +3,8 @@ import {View, Text, Image, ScrollView, TextInput, StyleSheet, Button} from 'reac
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View style={styles.container}> 
+        <View style={styles.container}>
+            <Image source={require('../image/Posturize-logos_black.png')} style={styles.image}/> 
             <Text>Welcome to Posturize!</Text>
             <Button
             title="Go to User Profile"
@@ -15,10 +16,19 @@ const HomeScreen = ({ navigation }) => {
     
 export default HomeScreen;
 const styles = StyleSheet.create({
+    image: {
+        padding: 20,
+        borderRadius: 15,
+        marginBottom: 20,
+        textAlign: 'center',
+        height: 150,
+        resizeMode: 'contain'
+      },
     container: {
       flex: 1,
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+      fontFamily: "Poppins-Bold",
     },
   });
--- screens/Profile.js
+++ screens/Profile.js
@@ -1,11 +1,45 @@
 import React from 'react'
-import {View, Text, Image, ScrollView, TextInput, StyleSheet, Button} from 'react-native'
+import {View, Text, Image, ScrollView, TextInput, StyleSheet, Button, TouchableOpacity} from 'react-native'
 
 function ProfileScreen() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Profile Screen</Text>
-      </View>
+        <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }} style={{ backgroundColor: '#292F3F', paddingTop: 20 }}>
+            <TextInput placeholder='First name' placeholderTextColor="#ffffff" style={styles.input}>Test</TextInput>
+            <TextInput placeholder='Last name' placeholderTextColor="#ffffff" style={styles.input}>Test</TextInput>
+            <TouchableOpacity
+            style={styles.userbutton}>
+            <Text style={styles.buttonText}>Update Profile</Text></TouchableOpacity>
+      </ScrollView>
     );
   }
   export default ProfileScreen;
+  const styles = StyleSheet.create({
+    input: {
+      width: "90%",
+      backgroundColor: `#373E4E`,
+      padding: 10,
+      marginBottom: 15,
+      color: 'white',
+      borderWidth: 1,
+      borderRadius: 15,
+      textAlign: 'center',
+      marginLeft: 20
+    },
+    buttonText: {
+      padding: 5,
+      color: 'black',
+      fontFamily: "Poppins-Bold",
+      textAlign: 'center',
+      fontSize: 15,
+    },
+    userbutton: {
+      backgroundColor: `#5fb7e3`,
+      padding: 10,
+      width: "45%",
+      marginTop: 5,
+      borderRadius: 20,
+      borderWidth: 1,
+      marginBottom: 40,
+      marginLeft: 110
+    },
+  })
