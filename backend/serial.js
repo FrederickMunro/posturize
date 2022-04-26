5a0c566feea714d0dfd8dcfee9b1b4945dead4bb
TysonPham <TysonPham96@gmail.com>
2022-04-26 14:42:13 -0400

    git rebasing from master

--- backend/serial.js
+++ backend/serial.js
@@ -1,23 +1,57 @@
-const { SerialPort } = require('serialport')
-const { ByteLengthParser } = require('@serialport/parser-byte-length')
-// Create a port
-const port = new SerialPort({
-  path: '/dev/cu.usbserial-DN01EYVM',
-  baudRate: 9600,
-})
 
-port.write('main screen turn on', function(err) {
-    if (err) {
-      return console.log('Error on write: ', err.message)
-    }
-    console.log('message written')
-  })
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+      io.emit('chat message', "hey appended")
+    });
+  });
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+
+
+
+
+// const { SerialPort } = require('serialport')
+// // Create a port
+// const serialport = new SerialPort({
+//   path: '/dev/cu.usbserial-DN01EYVM',
+//   baudRate: 9600,
+// })
+
+// serialport.write('main screen turn on', function(err) {
+//     if (err) {
+//       return console.log('Error on write: ', err.message)
+//     }
+//     console.log('message written')
+//   })
   
-  // Open errors will be emitted as an error event
-  port.on('error', function(err) {
-    console.log('Error: ', err.message)
-  })
+//   // Open errors will be emitted as an error event
+//   serialport.on('error', function(err) {
+//     console.log('Error: ', err.message)
+//   })
 
-  port.on('data', function (data) {
-    console.log(data)
-  })
+//   serialport.on('data', function (data) {
+//     console.log('Data:', data)
+    
+//   })
+
+
+
