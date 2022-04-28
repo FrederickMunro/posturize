130d63e1dfe16666867d32978ac9b4f431fa43a5
TysonPham <TysonPham96@gmail.com>
2022-04-28 13:02:51 -0400

    implemented websocket to show on browser, [CHORE] implement into react native app

--- backend/serial.js
+++ backend/serial.js
@@ -7,51 +7,57 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+
+//serving a server 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
+  
 
 
   io.on('connection', (socket) => {
+
+   
+
     socket.on('chat message', (msg) => {
+        
       io.emit('chat message', msg);
-      io.emit('chat message', "hey appended")
+      console.log(msg)
+      
     });
+
+
+
   });
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
 
 
 
 
 
 
-// const { SerialPort } = require('serialport')
-// // Create a port
-// const serialport = new SerialPort({
-//   path: '/dev/cu.usbserial-DN01EYVM',
-//   baudRate: 9600,
-// })
+  const { SerialPort } = require('serialport')
+const { ReadlineParser } = require('@serialport/parser-readline')
+const parser = new ReadlineParser()
+const port = new SerialPort({
+  path: '/dev/cu.usbserial-DN01EYVM',
+  baudRate: 9600,
+  databits: 8, 
+  parity:'none',
+  stopBits:1, 
+  flowControl: false
+})
 
-// serialport.write('main screen turn on', function(err) {
-//     if (err) {
-//       return console.log('Error on write: ', err.message)
-//     }
-//     console.log('message written')
-//   })
-  
-//   // Open errors will be emitted as an error event
-//   serialport.on('error', function(err) {
-//     console.log('Error: ', err.message)
-//   })
-
-//   serialport.on('data', function (data) {
-//     console.log('Data:', data)
+port.pipe(parser);
+parser.on('data', function(data) {
     
-//   })
-
+    console.log('Received data from port: ' + data);
+    io.emit('data', data);
+      
+  });
+  
 
 
